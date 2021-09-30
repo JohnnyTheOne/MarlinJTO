3268ffc822fbfa009117cd2503e200860b12b1b6
Johnny Eshak <info@johnnytheone.com>
2021-09-30 15:25:54 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -807,8 +807,7 @@
   //#define ENDSTOPPULLUP_JMAX
   //#define ENDSTOPPULLUP_KMAX
   #define ENDSTOPPULLUP_ZMIN_PROBE
-// Enable pulldown for all endstops to prevent a floating state
-//#define ENDSTOPPULLDOWNS
+
 #if DISABLED(ENDSTOPPULLDOWNS)
   // Disable ENDSTOPPULLDOWNS to set pulldowns individually
   //#define ENDSTOPPULLDOWN_XMIN
